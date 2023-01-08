2bad6f397339601a70ef046d8f147195e28f02a0
candacemonster <112183197+candacemonster@users.noreply.github.com>
2023-01-07 20:42:50 -0800

    Updated links, js code to copy email to clipboard and some responsive css adjustments.

--- src/main.js
+++ src/main.js
@@ -1,33 +1,27 @@
-// Add an event listener to the link to show the image and paragraph when clicked
-weatherLink.addEventListener("click", function (event) {
-  event.preventDefault();
-  weatherImage.style.display = "block";
-  weatherParagraph.style.display = "block";
-});
-
+// Add an event listener to the link to copy the text to the clipboard when clicked
 const copyLink = document.getElementById("copy-link");
 
 copyLink.addEventListener("click", (event) => {
   // Prevent the default link behavior
   event.preventDefault();
 
-  // Get the email address from the data-clipboard-text attribute
-  const email = copyLink.getAttribute("data-clipboard-text");
+  // Get the text to copy from the data-clipboard-text attribute
+  const textToCopy = copyLink.getAttribute("data-clipboard-text");
 
   // Create a temporary textarea element
   const tempTextarea = document.createElement("textarea");
-  tempTextarea.value = email;
+  tempTextarea.value = textToCopy;
   document.body.appendChild(tempTextarea);
 
-  // Select the email in the textarea
+  // Select the text in the textarea
   tempTextarea.select();
 
-  // Copy the email to the clipboard
+  // Copy the text to the clipboard
   document.execCommand("copy");
 
   // Remove the textarea element
   document.body.removeChild(tempTextarea);
 
-  // Update the link text to show that the email has been copied
+  // Update the link text to show that the text has been copied
   copyLink.textContent = "copied!";
 });
